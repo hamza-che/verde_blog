bcad6bf9785ca54886c228a127b44f6a9c214682
hamza-che <chenguitiha@gmail.com>
2022-08-15 13:19:12 +0100

    initialization of the project using webpack

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-primary text-4xl font-bold">
+        Hello, this is Verde Blog
+      </h1>
+    </div>
+  );
+};
+
+export default App;
